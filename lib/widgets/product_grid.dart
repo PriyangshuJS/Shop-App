24723a65c8be8846a2a286240616fe67d5760e6a
Priyangshu <pjscse@gmail.com>
2023-04-12 23:15:54 +0530

    4th

---
+++ lib/widgets/product_grid.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../providers/products_provider.dart';
+import 'product_struct.dart';
+
+class ProdutGrid extends StatefulWidget {
+  final bool showFav;
+  const ProdutGrid(this.showFav, {super.key});
+
+  @override
+  State<ProdutGrid> createState() => _ProdutGridState();
+}
+
+class _ProdutGridState extends State<ProdutGrid> {
+  @override
+  Widget build(BuildContext context) {
+    final productsData = Provider.of<Products>(context);
+    final products =
+        widget.showFav ? productsData.favoriteItems : productsData.item;
+    // print("\@nd -\n");
+    // print(productsData.favoriteItems.map((e) => e.title));
+    return GridView.builder(
+      padding: const EdgeInsets.all(10),
+      itemCount: products.length,
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        childAspectRatio: 3 / 2,
+        crossAxisSpacing: 10,
+        mainAxisSpacing: 10,
+      ),
+      itemBuilder: (ctx, index) => ChangeNotifierProvider.value(
+        // create: (c) => products[index],
+        value: products[index],
+        child: const ProductStructure(),
+      ),
+    );
+  }
+}
